689722d103aaf2a040101fc0797518caa9865e47
Lucas Antenor Da Silva Ribeiro <lucasda-@c1r10s9.42porto.com>
2023-04-18 18:31:44 +0100

    libft.h insert atoi

--- Libft/libft.h
+++ Libft/libft.h
@@ -24,6 +24,7 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+int		ft_atoi(const char *str);
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 
